0831e242128b452f9c698dd18bc0b06fd77389b2
Luke Lalor <lukehlalor@gmail.com>
2024-11-15 23:50:08 -0800

    add hashes script

---
+++ scripts/update-hashes.js
@@ -0,0 +1,37 @@
+import fs from 'node:fs';
+import yaml from 'js-yaml';
+import crypto from 'node:crypto';
+
+const fileContents = fs.readFileSync('invites.yaml', 'utf8');
+const data = yaml.load(fileContents);
+const usedHashes = new Set();
+
+// Update hashes where missing and reorder properties
+data.invites = data.invites.map(invite => {
+  if (!invite.hash) {
+    let hash;
+    do {
+      hash = crypto
+        .createHash('md5')
+        .update(invite.invite_key)
+        .digest('hex')
+        .slice(0, 8);
+    } while (usedHashes.has(hash));
+    invite.hash = hash;
+  }
+  usedHashes.add(invite.hash);
+
+  return {
+    hash: invite.hash,
+    invite_key: invite.invite_key,
+    emails: invite.emails,
+    guests: invite.guests
+  };
+});
+
+fs.writeFileSync(
+  'invites.yaml',
+  yaml.dump(data, { quotingType: '"', lineWidth: -1 })
+);
+
+console.log('Updated hashes in invites.yaml');
