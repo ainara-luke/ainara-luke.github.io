6033ab5640f8fb370b4106f910aa84a4fdd0b0b1
Luke Lalor <lukehlalor@gmail.com>
2024-11-16 17:19:00 -0800

    also push to viariable

--- scripts/update-github-variable.js
+++ scripts/update-github-variable.js
@@ -7,7 +7,14 @@ const REPO = 'ainara-luke/ainara-luke.github.io';
 try {
   // Read and update the config
   const configContent = readFileSync(CONFIG_PATH, 'utf8');
+
+  // Update the secret for github actions
   execSync(`gh secret set WEDDING_CONFIG -b"${configContent}" -R ${REPO}`, {
+    stdio: 'inherit'
+  });
+
+  // Set a variable in case we need access to the config data
+  execSync(`gh variable set WEDDING_CONFIG -b"${configContent}" -R ${REPO}`, {
     stdio: 'inherit'
   });
 
