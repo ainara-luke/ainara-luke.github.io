b7c3bef712e1cd16c8c43f7afb8cb5dbeb62ca28
Luke Lalor <lukehlalor@gmail.com>
2024-12-28 11:14:13 -0800

    localize nav

---
+++ src/i18n/nav.ts
@@ -0,0 +1,13 @@
+// src/i18n/nav.ts
+export const nav = {
+  en: {
+    home: "Home",
+    activities: "Activities",
+    about: "About"
+  },
+  es: {
+    home: "Inicio",
+    activities: "Actividades",
+    about: "Sobre Nosotros"
+  }
+} as const;
