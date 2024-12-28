e4c7f16c5af23e66c5c4452f29ba0eea72274342
Luke Lalor <lukehlalor@gmail.com>
2024-12-28 11:50:12 -0800

    enable rsvp + last of spanish translation

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const SITE: Site = {
   postPerIndex: 4,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-  showRSVP: false,
+  showRSVP: true,
   editPost: {},
 };
 
